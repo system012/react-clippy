692057b04329aa72a3d8c16bd6a53a647862e231
system <kory93@gmail.com>
2021-12-22 15:35:59 +0100

    added comments to url detection and redirection

--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,13 +1,13 @@
 import { Outlet, useParams } from "react-router-dom";
 import { NotFound } from "./NotFound";
 
-const isUrl = (value) => {
+const isUrl = (value) => {// se viene fornito un link valido verrà restituito un oggetto contenente isValidUrl (uguale a true) e l'url stesso
 
     try {
         const url = new URL(value)
         return {isValidUrl: true, url: url}
 
-    } catch (e) {
+    } catch (e) {// se l'url non è valido il costruttore URL triggera un errore che viene gestito, e si ritornerà un oggetto contentente isValidUrl = false
         return {isValidUrl: false, url: undefined}
     }
 }
@@ -19,7 +19,7 @@ export const ProtectedRoutes = () => {
         const decodedData = atob(data);
         const { time, value } = JSON.parse(decodedData)
         const {isValidUrl, url} = isUrl(value)
-        if (isValidUrl) return window.location.replace(url)
+        if (isValidUrl) return window.location.replace(url) // se il link fornito è valido la pagina reindirizza automaticamente l'utente al link stesso
 
         return <Outlet context={[time, value]} />
 
